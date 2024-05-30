00e6c1603d1786d4bd4900f8af13daa526eb31c5
atiohaidar <66902140+atiohaidar@users.noreply.github.com>
2024-05-31 02:12:14 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,7 +5,7 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		Counter counter = new Counter();
-		
+		// emang iya?
 		System.out.println("Current count:" + counter.getCount());
 		
 		counter.increment();
